47ac9a36152002ac823a6b94132fd73673177b74
Zoranais <unikronusus@gmail.com>
2023-10-25 23:46:23 +0300

    Add project files.

---
+++ lab1/LExpression.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1;
+public class LExpression
+{
+    public double A { get; private set; }
+    public double B { get; private set; }
+    public double C { get; private set; }
+
+    public LExpression()
+    {
+        
+        A = Random.Shared.Next(1, 10);
+        B = Random.Shared.Next(1, 10);
+        C = Random.Shared.Next(1, 10);
+    }
+
+    public LExpression(double a)
+    {
+        A = a;
+        B = Random.Shared.Next(1, 10);
+        C = Random.Shared.Next(1, 10);
+    }
+
+    public LExpression(double a, double b, double c)
+    {
+        A = a;
+        B = b;
+        C = c;
+    }
+
+    public double GetDiscriminant()
+    {
+        return Math.Pow(B, 2) - 4 * A * C;
+    }
+
+    public double[] Calculate()
+    {
+        double discriminant = GetDiscriminant();
+        if (discriminant > 0)
+        {
+            double root1 = (-B + Math.Sqrt(discriminant)) / (2 * A);
+            double root2 = (-B - Math.Sqrt(discriminant)) / (2 * A);
+            return new double[] { root1, root2 };
+        }
+        else if (discriminant == 0)
+        {
+            double root = -B / (2 * A);
+            return new double[] { root };
+        }
+        return new double[] { };
+    }
+}
---
+++ lab1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab1;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        double a, b, c;
+        try
+        {
+            a = Convert.ToDouble(AInput.Text);
+            b = Convert.ToDouble(BInput.Text);
+            c = Convert.ToDouble(CInput.Text);
+        }
+        catch (Exception)
+        {
+            MessageBox.Show("Invalid input");
+            return;
+        }
+        var expression = new LExpression(a, b, c);
+
+        var result = expression.Calculate();
+        ResultOutput.Content = result.Length == 0 ? "0 answers" : string.Join(", ", result);
+    }
+}
